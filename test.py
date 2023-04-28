7dede892dd913ce111a3a32c0fcb853e3739d85b
premagorn <76697806+premagorn@users.noreply.github.com>
2023-04-28 12:58:36 +0700

    test2

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 def flaoting_num(arr1, tar):
     ans = 0
     for i in range(len(arr1)):
-        for j in range(len(arr1)):
-            if arr1[i] + arr1[j] == tar:
-                ans = int(i + j)
+        for j in range(len(arr1) - i):
+            if arr1[i] + arr1[i + j] == tar:
+                ans = int(i + (i + j))
     return ans
 
 def main():
-    tar = int(input("Enter Target: "))
+    tar = float(input("Enter Target: "))
     nums = [2.5, 7.5, 11.2, 15.1]
     ans = flaoting_num(nums, tar)
     print(f'Answer: {ans}')
